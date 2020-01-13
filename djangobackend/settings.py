a726d4b5acc89b4402e0629dcf299ddd784dc1ff
ALEJ4NDRO29 <josealejandro.r.29@gmail.com>
2020-01-13 20:58:22 +0100

    Db config

--- djangobackend/settings.py
+++ djangobackend/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'djangobackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql', 
+        'NAME': 'djangodb',
+        'USER': 'django',
+        'PASSWORD': 'hola123',
+        'HOST': 'postgresdjango',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
