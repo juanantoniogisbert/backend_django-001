687b8c64576db72ac261a9cfa7387912785c8d6d
Juan Antonio <juagisla@gmail.com>
2020-01-28 21:36:06 +0100

    Created authentication app

--- djangobackend/settings.py
+++ djangobackend/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'hotel.apps.HotelConfig',
     'clients.apps.ClientsConfig',
+    'authentication.apps.AuthenticationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
