339e66dd534adee43952cb9fe76b7b5698ce56bf
ALEJ4NDRO29 <josealejandro.r.29@gmail.com>
2020-02-10 21:09:29 +0100

    Refactor CommentListApi

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 
-from .views import HotelViewSetAdmin, CommentViewSetAdmin, HotelViewSet, CommentsListCreateAPIView
+from .views import HotelViewSetAdmin, CommentViewSetAdmin, HotelViewSet, CommentsListAPIView
 
 router = DefaultRouter()
 router.register(r'hotels_Admin', HotelViewSetAdmin)
@@ -11,5 +11,5 @@ router.register('', HotelViewSet)
 
 urlpatterns = [
     url('', include(router.urls)),
-    url(r'^(?P<hotel_id>[0-9a-zA-Z_-]+)/comments', CommentsListCreateAPIView.as_view()),
+    url(r'^(?P<hotel_id>[0-9a-zA-Z_-]+)/comments', CommentsListAPIView.as_view()),
 ]
